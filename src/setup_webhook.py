77c7fa16fb94eaa605f081fbc7ae1f492adfc7ad
Brendan Devenney <brendan@devenney.io>
2018-04-13 12:31:44 +0100

    Refactor to Serverless Framework (#16)
    * Initial refactor to Serverless
    
    * Refactor testing framework.

---
+++ src/setup_webhook.py
@@ -0,0 +1,16 @@
+import os
+from pprint import pprint
+
+import requests
+
+tg_sec = os.environ['TELEGRAM_SEC']
+bot_token = os.environ['TELEGRAM_TOKEN']
+test_url = "https://bot.devenney.io/{}/roll".format(tg_sec)
+
+def get_url(method):
+    return "https://api.telegram.org/bot{}/{}".format(bot_token,method)
+
+r = requests.get(get_url("setWebhook"), data={"url": test_url})
+r = requests.get(get_url("getWebhookInfo"))
+pprint(r.status_code)
+pprint(r.json())
---
+++ tests/test_handler.py
@@ -0,0 +1,134 @@
+import json
+import logging
+import os
+import sys
+import unittest
+
+from unittest import mock
+
+sys.path.insert(0, './src')
+from handler import roll
+
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+
+def mock_tg_resp(*args, **kwargs):
+    class MockResponse:
+        def __init__(self, json_data, status_code):
+            self.json_data = json_data
+            self.text = {
+                "ok": True
+            }
+            self.status_code = status_code
+
+        def json(self):
+            return self.json_data
+
+    if args[0] == "https://api.telegram.org/botTEST":
+        return MockResponse(
+            {
+                "statusCode": "200",
+            },
+
+            200
+        )
+
+    return MockResponse(None, 404)
+
+
+class QueryTest(unittest.TestCase):
+    @mock.patch('requests.post', side_effect=mock_tg_resp)
+    def test(self, mock):
+        event = {}
+        event["body"] = json.dumps(
+            {
+                'update_id': 74399407,
+                'message': {
+                    'message_id': 614,
+                    'from': {
+                        'id': 339873294,
+                        'is_bot': False,
+                        'first_name': 'Brendan',
+                        'last_name': 'Devenney',
+                        'username': 'devenney',
+                        'language_code': 'en-US'
+                    },
+                    'chat': {
+                        'id': 339873294,
+                        'first_name': 'Brendan',
+                        'last_name': 'Devenney',
+                        'username': 'devenney',
+                        'type': 'private'
+                    },
+                    'date': 1523568333,
+                    'text': '/roll 2d20',
+                    'entities': [
+                        {
+                            'offset': 0,
+                            'length': 5,
+                            'type': 'bot_command'
+                        }
+                    ]
+                }
+            }
+        )
+
+        response = roll(
+            event,
+            None
+        )
+
+        logger.debug(response)
+        self.assertIsNotNone(response)
+
+class InlineTest(unittest.TestCase):
+    @mock.patch('requests.post', side_effect=mock_tg_resp)
+    def test(self, mock):
+        event = {}
+        event["body"] = json.dumps(
+            {
+                'update_id': 362761695,
+                'inline_query':
+                {
+                    'id': '1459744682983739794',
+                    'from': {
+                        'id': 339873294,
+                        'is_bot': False,
+                        'first_name': 'Brendan',
+                        'last_name': 'Devenney',
+                        'username': 'devenney',
+                        'language_code': 'en-US'
+                    },
+                    'query': '2d20 + 1d20 + 4',
+                    'offset': ''
+                }
+            }
+        )
+
+        response = roll(
+            event,
+            None
+        )
+
+        logger.debug(response)
+        self.assertIsNotNone(response)
+
+class GarbageTest(unittest.TestCase):
+    def test(self):
+        event = {}
+        event["body"] = json.dumps(
+            {
+                'update_id': 36212342,
+                'message': 'garbage'
+            }
+        )
+
+        response = roll(
+            event,
+            None
+        )
+
+        logger.debug(response)
+        self.assertIsNotNone(response)
